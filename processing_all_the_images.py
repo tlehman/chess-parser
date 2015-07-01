1778a6b4db0ecbac7bb02c529e5dd19b3c502a39
Tobi Lehman <tobi.lehman@gmail.com>
2015-07-01 12:54:47 -0700

    use random ints to split training and test

--- processing_all_the_images.py
+++ processing_all_the_images.py
@@ -3,15 +3,20 @@ import os
 import numpy as np
 import pandas as pd
 import pylab as pl
+from random import randint
 from sklearn.decomposition import RandomizedPCA
 from sklearn import svm
 
-img_train_dir = "ref/pieces/"
-img_test_dir = "tmp/027/"
+img_dir = "ref/pieces/"
 
-# the trainingset is an array of (vector,label) pairs
-images_training = [img_train_dir+f for f in os.listdir(img_train_dir)]
-images_test = [img_test_dir+f for f in os.listdir(img_test_dir)]
+images = [img_dir+f for f in os.listdir(img_dir)]
+# we want to train the classifier on 80% of the given data, and test on the other 20%
+training_or_test = [randint(0,100) < 80 for i in images]
+img_group = zip(images, training_or_test)
+images_training = filter(lambda p: p[1], img_group)
+images_test = filter(lambda p: not p[1], img_group)
+
+
 "0 is blank, odd positive numbers are black, even postives are white, etc."
 label_codes = {"_": 0, "BP": 1, "WP": 2,
                        "BR": 3, "WR": 4,
@@ -24,24 +29,20 @@ def label_from_code(c):
     return [label for label, code in label_codes.items() if code == c][0]
 
 
-labels = [f.split("/")[-1][:-7] for f in images_training]
+labels = [f[0].split("/")[-1][:-7] for f in images_training]
 target = [label_codes[label] for label in labels]
 
-data_training = []
-for image in images_training:
-    img = img_to_matrix(image)
-    img = flatten_image(img)
-    data_training.append(img)
+def images_to_vectors(images):
+    vectors = []
+    for image in images:
+        image = image[0]
+        img = img_to_matrix(image)
+        img = flatten_image(img)
+        vectors.append(img)
+    return np.array(vectors)
 
-data_test = []
-for image in images_test:
-    img = img_to_matrix(image)
-    img = flatten_image(img)
-    data_test.append(img)
-
-data_training = np.array(data_training)
-data_test = np.array(data_test)
-
+data_training = images_to_vectors(images_training)
+data_test = images_to_vectors(images_test)
 
 # using a randomized PCA, reducing to 25 dimensions (from 7500 dimensions)
 pca = RandomizedPCA(n_components=25)
