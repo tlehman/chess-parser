1ed515c7ea6d3acde19ad96eff55d4fa14155899
Tobi Lehman <tobi.lehman@gmail.com>
2015-07-01 13:18:33 -0700

    Tweak parameters, change to KNN

--- processing_all_the_images.py
+++ processing_all_the_images.py
@@ -5,7 +5,7 @@ import pandas as pd
 import pylab as pl
 from random import randint
 from sklearn.decomposition import RandomizedPCA
-from sklearn import svm
+from sklearn.neighbors import KNeighborsClassifier
 
 img_dir = "ref/pieces/"
 
@@ -44,17 +44,27 @@ def images_to_vectors(images):
 data_training = images_to_vectors(images_training)
 data_test = images_to_vectors(images_test)
 
-# using a randomized PCA, reducing to 25 dimensions (from 7500 dimensions)
-pca = RandomizedPCA(n_components=25)
+# using a randomized PCA, reducing to 13 dimensions (from 7500 dimensions)
+pca = RandomizedPCA(n_components=13)
 X = pca.fit_transform(data_training)
 X_test = pca.fit_transform(data_test)
 
 # make a linear support vector classifier
-clf = svm.LinearSVC()
-clf.fit(X, target)
+knn = KNeighborsClassifier()
+knn.fit(X, target)
 
 # check classified images
+number_correct = 0
+number_total = len(X_test)
 for i, x in enumerate(X_test):
-    predicted = clf.predict(x)[0]
+    predicted = knn.predict(x)[0]
     label = label_from_code(predicted)
-    print "%s  is  %s" % (images_test[i], label)
+    image = images_test[i][0]
+
+    correct = image.startswith(img_dir + label)
+    if correct:
+        number_correct += 1
+    print "%s  is  %s (%r)" % (image, label, correct)
+
+percent_correct = 100*float(number_correct)/float(number_total)
+print "%d correct out of %d, %d percent accurate" % (number_correct, number_total, percent_correct)
